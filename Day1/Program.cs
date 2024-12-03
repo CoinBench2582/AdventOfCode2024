ae9e3543b6cba9a966f7a50f8ff0734cfcf8b6e9
CoinBench <coinbench.riot@outlook.com>
2024-12-03 21:20:01 +0100

    Reorganisation

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -20,70 +20,57 @@ internal class Program
             return;
         }
 #endif
-        (int result, Exception? ex) = TryGetResult(path);
+        Exception? ex = TryGetResult(path, out int distance);
         if (ex is not null)
         {
             WriteLine($"Stala se chyba: {ex.Message}");
             return;
         }
-        WriteLine($"Výsledná vzdálenost: {result}");
+        WriteLine($"Výsledná vzdálenost: {distance}");
         ReadLine();
     }
 }
 
 public static class Methods
 {
-    public static (int, Exception?) TryGetResult(string pathOfSource)
+    public static Exception? TryGetResult(string pathOfSource, out int distance)
     {
         try
         {
-            var (left, right) = ParseData(pathOfSource);
-            int result = left.AggregateEasy(right);
-            return (result, null);
+            var (left, right) = ParseData(pathOfSource).SplitData();
+            distance = left.GetDistance(right);
+            return null;
         }
         catch (Exception ex)
         {
-            return (default, ex);
+            distance = default;
+            return ex;
         }
     }
 
-    public static int AggregateEasy(this IEnumerable<int> left, IEnumerable<int> right)
+    public static int GetDistance(this IEnumerable<int> left, IEnumerable<int> right)
         => left.Order().Zip(right.Order())
                 .Select(t => int.Abs(t.First - t.Second))
                 .Sum();
 
-    private static (int[] left, int[] right) ParseData(string aboslutePath)
+    public static int GetDistance(this IEnumerable<Pair> data)
+        => data.Select(t => int.Abs(t.Left - t.Right)).Sum();
+
+    internal static (IEnumerable<int> left, IEnumerable<int> right) SplitData(this IEnumerable<Pair> data)
     {
-        int[] left, right;
-
-        try
-        {
-            string[] lines = File.ReadAllLines(aboslutePath);
-            if (lines.Length == 0)
-                throw new IOException("Soubor je prázdný!");
-            (left, right) = (new int[lines.Length], new int[lines.Length]);
-            var reduced =
-                lines.Select(s => s.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
-                    .Select(x => x.Length == 2
-                        ? new Pair(int.Parse(x[0]), int.Parse(x[1]))
-                        : throw new ArgumentException($"Neočekávaná délka řádku: {x.Length}"));
-            int i = 0;
-            foreach (Pair item in reduced)
-            {
-                (left[i], right[i]) = (item.Left, item.Right);
-                i++;
-            }
-        } 
-        catch (Exception ex)
-        {
-#if DEBUG
-            WriteLine(ex.ToString());
-#endif
-            throw;
-        }
-
+        int count = data.Count();
+        (List<int> left, List<int> right) = (new(++count), new(count));
+        left.AddRange(data.Select(p => p.Left));
+        right.AddRange(data.Select(p => p.Right));
         return (left, right);
     }
 
-    internal record struct Pair(int Left, int Right);
+    private static IEnumerable<Pair> ParseData(string aboslutePath)
+        => File.ReadAllLines(aboslutePath)
+            .Select(s => s.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => x.Length == 2
+                ? new Pair(int.Parse(x[0]), int.Parse(x[1]))
+                : throw new ArgumentException($"Neočekávaná délka řádku: {x.Length}"));
 }
+
+public record struct Pair(int Left, int Right);
