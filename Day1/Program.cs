4df631d6096834734a1f390a0f6d0c184a69c691
CoinBench <coinbench.riot@outlook.com>
2024-12-03 17:13:12 +0100

    Added path parsing

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -6,7 +6,20 @@ internal class Program
 {
     static void Main()
     {
-        
+        string path;
+#if DEBUG
+        const string localPath = @"D:\VOJTA\Moje\C_Sharp\AdventOfCode2024\Day1\Source.txt";
+        path = localPath;
+#else
+        WriteLine($"Vložte úplnou cestu k cílovému souboru");
+        path = ReadLine() ?? string.Empty;
+        if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+        {
+            WriteLine("Cesta není platná!");
+            return;
+        }
+#endif
+
     }
 }
 
