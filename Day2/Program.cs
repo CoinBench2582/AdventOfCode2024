7960540eb7edd91f6e7134e7c4841586002705f1
CoinBench <coinbench.riot@outlook.com>
2024-12-04 17:38:34 +0100

    Create Day2 project
    Signed-off-by: CoinBench <coinbench.riot@outlook.com>

---
+++ Day2/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+using static Day2.Methods;
+
+namespace Day2;
+
+internal class Program
+{
+#if DEBUG
+    const string localPath = @"Source.txt";
+    static readonly string debugPath = Path.GetFullPath(localPath);
+#endif
+
+    static void Main()
+    {
+        string path;
+#if DEBUG
+        path = debugPath;
+#else
+        WriteLine($"Vložte úplnou cestu k cílovému souboru");
+        path = ReadLine() ?? string.Empty;
+        if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+        {
+            WriteLine("Cesta není platná!");
+            return;
+        }
+#endif
+    }
+}
+
+public static class Methods
+{
+
+}
