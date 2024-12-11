c573c656c1708695c251eab7c5eeb38e509b952b
CoinBench2582 <coinbench.riot@outlook.com>
2024-12-11 11:56:00 +0100

    Založení projektu pro třetí den

---
+++ Day3/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+using static Day3.Methods;
+
+namespace Day3
+{
+    internal class Program
+    {
+#if DEBUG
+        const string test = @"Test.text";
+        const string target = @"Source.txt";
+        readonly static string localPath = Path.GetFullPath(test);
+#endif
+
+        static void Main()
+        {
+            string path;
+#if DEBUG
+            path = localPath;
+#else
+            WriteLine($"Vložte úplnou cestu k cílovému souboru");
+            path = ReadLine() ?? string.Empty;
+            if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+            {
+                WriteLine("Cesta není platná!");
+                return;
+            }
+#endif
+            Exception? ex = TryGetResult(path);
+            if (ex is not null)
+            {
+                WriteLine($"Stala se chyba: {ex.Message}");
+                return;
+            }
+            WriteLine($"");
+            _ = ReadLine();
+        }
+    }
+
+    public static class Methods
+    {
+        public static Exception? TryGetResult(string path)
+        {
+            return new NotImplementedException();
+        }
+
+
+        #region Part 1
+
+        #endregion
+
+        #region Utils
+
+        #endregion
+    }
+}
